465593f034d2c1b78b44a0fb0aab1a08e534d0c6
Your Name <you@example.com>
2021-04-30 01:44:35 +0900

    Configure Storybook sass

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,21 @@
+const path = require('path');
+
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
+  ],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    return config;
+  },
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import 'destyle.css';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
