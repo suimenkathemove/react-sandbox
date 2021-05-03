f6ceaa0799245b50b89b2f2d67a35900c9e595ad
Your Name <you@example.com>
2021-05-03 22:47:31 +0900

    no-non-null-assertion off

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,6 +45,7 @@ module.exports = {
     "@typescript-eslint/ban-ts-comment": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/no-unused-vars": [
       "error",
       {
