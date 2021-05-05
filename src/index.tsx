a36c3aea2ff9aefb4fcd21a38c6803f8cc5dc25a
Your Name <you@example.com>
2021-05-05 16:23:02 +0900

    Add destyle.css to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import App from "@/App";
 import reportWebVitals from "@/reportWebVitals";
 import React from "react";
 import ReactDOM from "react-dom";
+import "destyle.css";
 
 ReactDOM.render(
   <React.StrictMode>
