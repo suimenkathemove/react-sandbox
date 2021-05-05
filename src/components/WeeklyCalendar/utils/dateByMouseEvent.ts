e6f3ee5a47baa6dbebe72cabf547213d7806c25e
Your Name <you@example.com>
2021-05-05 15:15:10 +0900

    Refactor dateByMouseEvent

--- src/components/WeeklyCalendar/utils/dateByMouseEvent.ts
+++ src/components/WeeklyCalendar/utils/dateByMouseEvent.ts
@@ -2,6 +2,8 @@ import { setTime } from "@/utils/date/setTime";
 import { mouseCoordinate } from "@/utils/mouseCoordinate";
 import { HOUR_HEIGHT } from "./hourHeight";
 
+const FIFTEEN_MINUTES_HEIGHT = HOUR_HEIGHT / (60 / 15);
+
 export const dateByMouseEvent = (
   mouseEvent: React.MouseEvent<HTMLDivElement, MouseEvent>,
   date: Date,
@@ -9,7 +11,7 @@ export const dateByMouseEvent = (
   const mouseY = mouseCoordinate.yFromElementTop(mouseEvent);
   const hours = Math.floor(mouseY / HOUR_HEIGHT);
   const minutes =
-    Math.floor((mouseY - HOUR_HEIGHT * hours) / (HOUR_HEIGHT / (60 / 15))) * 15;
+    Math.floor((mouseY - HOUR_HEIGHT * hours) / FIFTEEN_MINUTES_HEIGHT) * 15;
 
   return setTime(date, hours, minutes);
 };
