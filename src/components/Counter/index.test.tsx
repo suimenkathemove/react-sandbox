0a1a88d7440200043a169990725cf4db09bed66c
Your Name <you@example.com>
2021-05-13 16:01:13 +0900

    Add Counter test

---
+++ src/components/Counter/index.test.tsx
@@ -0,0 +1,23 @@
+import ReactDOM from "react-dom";
+import { Counter } from "./";
+
+beforeEach(() => {
+  document.body.innerHTML = "";
+});
+
+test("counter increments and decrements when the buttons are clicked", () => {
+  const div = document.createElement("div");
+  document.body.append(div);
+  ReactDOM.render(<Counter />, div);
+
+  const [decrement, increment] = div.querySelectorAll("button");
+  const count = div.querySelector("span");
+
+  expect(count!.textContent).toBe("0");
+
+  increment.click();
+  expect(count!.textContent).toBe("1");
+
+  decrement.click();
+  expect(count!.textContent).toBe("0");
+});
