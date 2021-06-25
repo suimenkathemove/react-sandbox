3288727aa5a7023ade8d1019d6dedda1a82eff94
Your Name <you@example.com>
2021-06-25 23:35:17 +0900

    Setup wasm

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,7 +1,13 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    unsafe {
+        alert(&format!("Hello, {}!", name));
     }
 }
