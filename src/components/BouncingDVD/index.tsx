991e0ece38a9cd7a8dce0af795bc57c63b713ae2
Your Name <you@example.com>
2021-07-05 00:26:35 +0900

    Add fillRect

--- src/components/BouncingDVD/index.tsx
+++ src/components/BouncingDVD/index.tsx
@@ -1,4 +1,5 @@
-import { useRef } from "react";
+import { onUpdateFrame } from "@/utils/onUpdateFrame";
+import { useEffect, useRef } from "react";
 import styles from "./styles.module.scss";
 import { useSpread } from "./utils/useSpread";
 
@@ -6,9 +7,24 @@ export const BouncingDVD: React.VFC = () => {
   const rootRef = useRef<HTMLDivElement>(null);
   const canvasSize = useSpread(rootRef);
 
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const getContext = () => canvasRef.current?.getContext("2d");
+
+  useEffect(() => {
+    const ctx = getContext()!;
+
+    onUpdateFrame(() => {
+      ctx.fillRect(0, 0, canvasSize.width, canvasSize.height);
+    });
+  }, [canvasSize.height, canvasSize.width]);
+
   return (
     <div ref={rootRef} className={styles.root}>
-      <canvas width={canvasSize.width} height={canvasSize.height} />
+      <canvas
+        ref={canvasRef}
+        width={canvasSize.width}
+        height={canvasSize.height}
+      />
     </div>
   );
 };
