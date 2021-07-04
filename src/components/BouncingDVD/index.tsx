6e56214f6f5bc404db172a20b7ed4cde37dcbf14
Your Name <you@example.com>
2021-07-04 23:28:54 +0900

    Spread to fill screen

--- src/components/BouncingDVD/index.tsx
+++ src/components/BouncingDVD/index.tsx
@@ -1,3 +1,23 @@
+import { useEffect, useRef, useState } from "react";
+import styles from "./styles.module.scss";
+
 export const BouncingDVD: React.VFC = () => {
-  return <canvas />;
+  const rootRef = useRef<HTMLDivElement>(null);
+
+  const [canvasSize, setCanvasSize] = useState<{
+    width: number;
+    height: number;
+  }>({ width: 0, height: 0 });
+  useEffect(() => {
+    setCanvasSize({
+      width: rootRef.current!.clientWidth,
+      height: rootRef.current!.clientHeight,
+    });
+  }, []);
+
+  return (
+    <div ref={rootRef} className={styles.root}>
+      <canvas width={canvasSize.width} height={canvasSize.height} />
+    </div>
+  );
 };
