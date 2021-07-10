2fabf7a1d5288fc0f1b052aa127c2d487ec5a018
Your Name <you@example.com>
2021-07-10 21:29:03 +0900

    Add key to objMap fn

--- src/utils/objMap.ts
+++ src/utils/objMap.ts
@@ -6,12 +6,12 @@ type RecursiveObj = {
 
 export const objMap = (
   obj: RecursiveObj,
-  fn: (val: unknown) => unknown,
+  fn: (val: unknown, key: keyof typeof obj) => unknown,
 ): RecursiveObj =>
   Object.keys(obj).reduce((acc, key) => {
     const val = obj[key];
 
-    return { ...acc, [key]: isObject(val) ? objMap(val, fn) : fn(val) };
+    return { ...acc, [key]: isObject(val) ? objMap(val, fn) : fn(val, key) };
   }, {});
 
 const convertEmptyStringToNullRecursively = (obj: RecursiveObj): RecursiveObj =>
