fff0d27de2390964831acf4b176d7bc5d95ed770
Your Name <you@example.com>
2021-07-14 00:03:29 +0900

    Add Input

---
+++ src/components/Trello/Input/index.tsx
@@ -0,0 +1,10 @@
+import styles from "./styles.module.scss";
+
+type Props = React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>;
+
+export const Input: React.VFC<Props> = (props) => {
+  return <input className={styles.base} {...props} />;
+};
