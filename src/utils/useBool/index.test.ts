d63ec17021d6eb67fa9e505adad18cbf55ae6a58
emgniddikur <yoshiharu2580@gmail.com>
2021-11-22 01:03:19 +0900

    Add useBool

---
+++ src/utils/useBool/index.test.ts
@@ -0,0 +1,18 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import { useBool } from "./";
+
+test("useBool", () => {
+  const { result } = renderHook(() => useBool());
+
+  expect(result.current[0]).toBe(false);
+
+  act(() => {
+    result.current[1]();
+  });
+  expect(result.current[0]).toBe(true);
+
+  act(() => {
+    result.current[2]();
+  });
+  expect(result.current[0]).toBe(false);
+});
---
+++ src/utils/useBool/index.ts
@@ -0,0 +1,16 @@
+import { useCallback, useState } from "react";
+
+export const useBool = (
+  initialValue = false
+): [boolean, VoidFunction, VoidFunction] => {
+  const [bool, setBool] = useState(initialValue);
+
+  const on = useCallback(() => {
+    return setBool(true);
+  }, []);
+  const off = useCallback(() => {
+    return setBool(false);
+  }, []);
+
+  return [bool, on, off];
+};
