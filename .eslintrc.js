608d69b14ffe08af50ee05b1a9f990d22ec389d9
emgniddikur <yoshiharu2580@gmail.com>
2021-11-26 03:16:08 +0900

    Add comment

--- .eslintrc.js
+++ .eslintrc.js
@@ -59,6 +59,7 @@ module.exports = {
       },
     ],
     "@typescript-eslint/ban-ts-comment": "off",
+    // TODO: remove
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
