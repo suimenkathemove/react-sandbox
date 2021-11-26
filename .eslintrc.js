e5679d97204c910324d87a399c3b9402911c8663
emgniddikur <yoshiharu2580@gmail.com>
2021-11-26 14:24:39 +0900

    Remove @typescript-eslint/explicit-module-boundary-types

--- .eslintrc.js
+++ .eslintrc.js
@@ -67,7 +67,6 @@ module.exports = {
       },
     ],
     "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/no-unused-vars": ["error"],
