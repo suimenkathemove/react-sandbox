cb0fceff6bb942580d2befe64ff0bfaa46615799
emgniddikur <yoshiharu2580@gmail.com>
2022-01-01 01:09:14 +0900

    Fix test

--- src/components/sortable-tree/index.test.tsx
+++ src/components/sortable-tree/index.test.tsx
@@ -2,7 +2,6 @@ import { cleanup, render } from '@testing-library/react';
 
 import { defaultProps } from './index.stories';
 
-import 'jest-styled-components';
 import { SortableTree } from './';
 
 const setup = () => render(<SortableTree {...defaultProps} />);
