a0bd0bce8e81bf994e0ba89b968638fb76f5c666
emgniddikur <yoshiharu2580@gmail.com>
2022-01-01 15:18:55 +0900

    Add Handle

--- src/components/sortable-tree/components/handle/index.tsx
+++ src/components/sortable-tree/components/handle/index.tsx
@@ -1,7 +1,18 @@
+import { memo } from 'react';
+
 import styles from './styles.module.scss';
 
-export type HandleProps = {};
+export type HandleProps = React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
 
-export const Handle: React.VFC<HandleProps> = (props) => {
-  return <></>;
-};
+export const Handle = memo<HandleProps>((props) => {
+  return (
+    <button className={styles.base} {...props}>
+      <svg viewBox="0 0 20 20" width="12">
+        <path d="M7 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 2zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 14zm6-8a2 2 0 1 0-.001-4.001A2 2 0 0 0 13 6zm0 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 14z"></path>
+      </svg>
+    </button>
+  );
+});
