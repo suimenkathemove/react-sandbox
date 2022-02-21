234bcef4910e96d9008bf5de9cf0b30e602ddab1
emgniddikur <yoshiharu2580@gmail.com>
2022-02-22 00:46:21 +0900

    Add mockdate

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,9 @@
 import 'destyle.css';
 import '@/styles/globals.scss';
 
+import MockDate from 'mockdate';
 import { withScreenshot } from 'storycap';
+
+MockDate.set('2022-01-01');
 
 export const decorators = [(Story) => <Story />, withScreenshot];
