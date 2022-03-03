463a6f886d7b6748f1ceb81eee0706b9eee1acc0
emgniddikur <yoshiharu2580@gmail.com>
2022-03-03 20:44:57 +0900

    Add TuplifyUnion

---
+++ src/@types/tuplify-union.d.ts
@@ -0,0 +1,8 @@
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (k: infer I) => void ? I : never;
+type LastOf<T> = UnionToIntersection<T extends any ? () => T : never> extends () => infer R ? R : never;
+
+type Push<T extends any[], V> = [...T, V];
+
+type TuplifyUnion<T, L = LastOf<T>, N = [T] extends [never] ? true : false> = true extends N
+  ? []
+  : Push<TuplifyUnion<Exclude<T, L>>, L>;
