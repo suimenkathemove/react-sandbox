f2d53f4a34862213954c6edc0f27bec1ce178710
emgniddikur <yoshiharu2580@gmail.com>
2022-03-07 22:07:08 +0900

    Add useStateWithReset

---
+++ src/hooks/use-state-with-reset/index.ts
@@ -0,0 +1,16 @@
+import { useCallback, useState } from 'react';
+
+export const useStateWithReset = <S>(
+  initialState: S,
+): [
+  state: S,
+  setState: React.Dispatch<React.SetStateAction<S>>,
+  resetState: VoidFunction,
+] => {
+  const [state, setState] = useState(initialState);
+  const resetState = useCallback(() => {
+    setState(initialState);
+  }, [initialState]);
+
+  return [state, setState, resetState];
+};
