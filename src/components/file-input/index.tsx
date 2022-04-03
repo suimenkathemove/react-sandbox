33a72c65bd0e3a541668c5cb29236d39a9a44b18
emgniddikur <yoshiharu2580@gmail.com>
2022-04-04 05:02:15 +0900

    fix

--- src/components/file-input/index.tsx
+++ src/components/file-input/index.tsx
@@ -6,22 +6,24 @@ export type FileInputProps = {
   onChange: (file: File) => void;
 };
 
-export const FileInput = forwardRef<HTMLInputElement, FileInputProps>((props, ref) => {
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { files } = event.target;
+export const FileInput = forwardRef<HTMLInputElement, FileInputProps>(
+  (props, ref) => {
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { files } = event.target;
 
-    if (files == null) {
-      return;
-    }
+      if (files == null) {
+        return;
+      }
 
-    const file = files[0];
+      const file = files[0];
 
-    if (file == null) {
-      return;
-    }
+      if (file == null) {
+        return;
+      }
 
-    props.onChange(file);
-  };
+      props.onChange(file);
+    };
 
-  return <Input ref={ref} type="file" onChange={onChange} />;
-});
+    return <Input ref={ref} type="file" onChange={onChange} />;
+  },
+);
