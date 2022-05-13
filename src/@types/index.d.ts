e5f35739b8a243e2beab3044e9e56ae5738017da
emgniddikur <yoshiharu2580@gmail.com>
2022-05-13 21:34:40 +0900

    add OnlyOne

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -2,5 +2,9 @@ type Fn = (...args: any[]) => any;
 
 type Maybe<T> = T | null;
 
+type OnlyOne<T, K = keyof T> = K extends keyof T
+  ? Pick<T, K> & { [P in Exclude<keyof T, K>]?: never }
+  : never;
+
 declare module '*.png';
 declare module '*.svg';
