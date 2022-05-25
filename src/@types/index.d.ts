0b468374c0500f00f19ea2fb3a0a46ba4da92370
emgniddikur <yoshiharu2580@gmail.com>
2022-05-25 10:18:52 +0900

    fix OnlyOne

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -2,7 +2,7 @@ type Fn = (...args: any[]) => any;
 
 type Maybe<T> = T | null;
 
-type OnlyOne<T, K = keyof T> = K extends keyof T
+type OnlyOne<T, K extends keyof T = keyof T> = K extends keyof T
   ? Pick<T, K> & { [P in Exclude<keyof T, K>]?: never }
   : never;
 
