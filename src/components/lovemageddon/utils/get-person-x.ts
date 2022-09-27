923ab26cac1a9fd7e2732ea971b717e81ff37f32
emgniddikur <yoshiharu2580@gmail.com>
2022-09-27 09:53:33 +0900

    bugfix getPersonX, getPersonY

--- src/components/lovemageddon/utils/get-person-x.ts
+++ src/components/lovemageddon/utils/get-person-x.ts
@@ -1,4 +1,4 @@
 import { CANVAS_WIDTH } from '../constants';
 
 export const getPersonX = (personCount: number, personXRatio: number): number =>
-  CANVAS_WIDTH / ((personCount + 1) * personXRatio);
+  (CANVAS_WIDTH / (personCount + 1)) * personXRatio;
--- src/components/lovemageddon/utils/get-person-y.ts
+++ src/components/lovemageddon/utils/get-person-y.ts
@@ -1,4 +1,4 @@
 import { CANVAS_HEIGHT } from '../constants';
 
 export const getPersonY = (personYRatio: number): number =>
-  CANVAS_HEIGHT / (3 * personYRatio);
+  (CANVAS_HEIGHT / 3) * personYRatio;
