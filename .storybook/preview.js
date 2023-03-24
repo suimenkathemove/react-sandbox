24a01003801d4e5316a9647913715c23fab9d196
suimenkathemove <yoshiharu2580@gmail.com>
2023-03-24 20:51:00 +0900

    add nextui

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,21 @@
 import 'destyle.css';
 import '@/styles/globals.scss';
 
+import { NextUIProvider } from '@nextui-org/react';
 import MockDate from 'mockdate';
 import { withScreenshot } from 'storycap';
+
 import { worker } from '../src/mocks/browser';
 
 MockDate.set('2022-01-01');
 
-export const decorators = [(Story) => <Story />, withScreenshot];
+export const decorators = [
+  (Story) => (
+    <NextUIProvider>
+      <Story />
+    </NextUIProvider>
+  ),
+  withScreenshot,
+];
 
 worker.start();
