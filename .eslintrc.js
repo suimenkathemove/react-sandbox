f50b1723d2b0533f97ec764bbb205b89c561cbac
suimenkathemove <yoshiharu2580@gmail.com>
2024-01-04 22:00:42 +0900

    fix playwright

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,16 @@ module.exports = {
     // "@suimenkathemove/frontend-eslint-config",
     './node_modules/@suimenkathemove/frontend-eslint-config',
   ],
+  overrides: [
+    {
+      files: ['cypress/**/*.ts', 'cypress/**/*.tsx'],
+      parserOptions: {
+        project: './cypress/tsconfig.json',
+      },
+      rules: {
+        '@typescript-eslint/no-floating-promises': 'error',
+      },
+    },
+  ],
   ignorePatterns: ['wasm'],
 };
