602fd9d57aee94cd8098bd85b46cefe72b13f4e3
suimenkathemove <yoshiharu2580@gmail.com>
2024-02-09 01:50:43 +0900

    skip when move to descendant

--- src/components/gui/sortable-tree-like-notion/models.ts
+++ src/components/gui/sortable-tree-like-notion/models.ts
@@ -5,6 +5,7 @@ import {
 } from '@/components/gui/sortable-tree/types';
 import { buildTree } from '@/components/gui/sortable-tree/utils/build-tree';
 import { arrayMove } from '@/utils/array-move';
+import { invariant } from '@/utils/invariant';
 
 export type BorderOrBackground =
   | {
@@ -19,6 +20,22 @@ export type BorderOrBackground =
       index: number;
     };
 
+const getDescendantIds = (
+  flattenedTree: FlattenedTreeItem[],
+  fromItem: FlattenedTreeItem,
+  fromIndex: number,
+): NodeId[] => {
+  const descendantIds: NodeId[] = [fromItem.id];
+  for (let i = fromIndex + 1; i < flattenedTree.length; i++) {
+    const item = flattenedTree[i];
+    invariant(item != null, 'item should exist');
+    if (item.depth <= fromItem.depth) break;
+    descendantIds.push(item.id);
+  }
+
+  return descendantIds;
+};
+
 export const sortTree = (
   flattenedTree: FlattenedTreeItem[],
   fromItem: FlattenedTreeItem,
@@ -26,6 +43,10 @@ export const sortTree = (
   fromIndex: number,
   toIndex: number,
 ): Tree => {
+  const descendantIds = getDescendantIds(flattenedTree, fromItem, fromIndex);
+  if (descendantIds.includes(newParentIdOfFromItem))
+    return buildTree(flattenedTree);
+
   const newFromItem: FlattenedTreeItem = {
     ...fromItem,
     parentId: newParentIdOfFromItem,
