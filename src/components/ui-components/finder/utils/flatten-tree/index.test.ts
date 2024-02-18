762456aba98ffde6a36b505b2a7ba4cf2d95886a
suimenkathemove <42399693+suimenkathemove@users.noreply.github.com>
2024-02-18 16:35:34 +0900

    delete finder (#54)

--- src/components/ui-components/finder/utils/flatten-tree/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { flattenedTree } from '../../__mocks__/flattened-tree';
-import { tree } from '../../__mocks__/tree';
-
-import { flattenTree } from './';
-
-test('flattenTree', () => {
-  expect(flattenTree(tree)).toEqual(flattenedTree);
-});
