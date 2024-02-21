192ee05817e2fb1446361c992bacab0eb1eb8f27
suimenkathemove <42399693+suimenkathemove@users.noreply.github.com>
2024-02-21 19:53:05 +0900

    add react-library (#57)

---
+++ bin/build.ts
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { build } = require('esbuild');
+
+void build({
+  entryPoints: [path.resolve(__dirname, '../src/index.ts')],
+  bundle: true,
+  outfile: 'dist/index.js',
+  platform: 'browser',
+  format: 'esm',
+  external: ['react', 'react-dom'],
+  minify: true,
+  sourcemap: true,
+});
