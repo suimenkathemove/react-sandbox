64fb524985b1e6ef5172becbe4d5c6a16e6d9530
suimenkathemove <yoshiharu2580@gmail.com>
2025-01-31 11:12:43 +0900

    fix

--- src/components/ui-components/video-player/index.tsx
+++ src/components/ui-components/video-player/index.tsx
@@ -1,5 +1,34 @@
+import { useRef, useState } from 'react';
+
+import videoFile from './assets/sample-video.mp4';
+
 type VideoPlayerProps = {};
 
 export const VideoPlayer: React.FC<VideoPlayerProps> = (props) => {
-  return <video />;
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  // MEMO: 実際の再生状態はvideoRef.current.pausedで取得できる
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const togglePlay = async () => {
+    if (!videoRef.current) {
+      // TODO
+      return;
+    }
+
+    if (videoRef.current.paused) {
+      await videoRef.current.play();
+      setIsPlaying(true);
+    } else {
+      videoRef.current.pause();
+      setIsPlaying(false);
+    }
+  };
+
+  return (
+    <div onClick={togglePlay}>
+      <video src={videoFile} ref={videoRef} />
+      <span>{isPlaying ? 'pause' : 'play'}</span>
+    </div>
+  );
 };
